d80b518f05b97a5c8e156bfad7609980ed05ea2f
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2022-04-14 10:22:26 -0700

    style: update type name

--- src/lib/util/isGraveyardAvailable.ts
+++ src/lib/util/isGraveyardAvailable.ts
@@ -1,9 +1,9 @@
 import type { RealmeyePlayer } from '../types/RealmeyePlayer';
 
-export function isGraveyardAvailable(player: RealmeyePlayer): player is GraveyardAvailable {
+export function isGraveyardAvailable(player: RealmeyePlayer): player is RealmeyePlayerWithGraveyard {
 	if (player.dungeonCompletions && Object.keys(player.dungeonCompletions).length) return true;
 
 	return false;
 }
 
-type GraveyardAvailable = RealmeyePlayer & { dungeonCompletions: Record<string, number> };
+type RealmeyePlayerWithGraveyard = RealmeyePlayer & { dungeonCompletions: Record<string, number> };

7ce053e1325dd6380a36b7fb59d0462b9727a169
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2022-04-14 10:22:31 -0700

    chore: add export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * from './lib/scrapeDungeons';
 export * from './lib/scrapePlayer';
+export * from './lib/player/scrapeDungeonCompletions'
 export * from './lib/scrapeGuild';
+
 
 export * from './lib/types/RealmeyePlayer';
 export * from './lib/types/RealmeyeGuild';
